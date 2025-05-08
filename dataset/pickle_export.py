23be4c525a9aba5676da631d5f95132a53eda300
Elena Ferro <elena.ferro@danfoss.com>
2025-05-08 15:18:32 +0200

    fix: pickle export

--- dataset/pickle_export.py
+++ dataset/pickle_export.py
@@ -1,37 +1,81 @@
 import os
 
+from datetime import datetime
 import pandas as pd
 from tqdm import tqdm
 import numpy as np
 
-from dataset import generate_datasets, load_miplib_dataset
+from sources.generator import generate_datasets
+from sources.miplib import load_miplib_dataset
 
 
-def load_and_export():
-    np.random.seed(42)
-    dataset = pd.DataFrame()
-    stats = pd.DataFrame()
+    
+def solve(problems, name):
+    dataset_name = f"dataset/{name}_solution.pkl"
+    stats_name = f"dataset/{name}_stats.pkl"
 
-    # miplib = load_miplib_dataset()
-    problems = generate_datasets(
-        set_cover_instances=0,
-        bin_packing_instances=1,
-        traveling_salesman_instances=1
-    )
-    for problem in problems['BP']:
+    dataset = pd.read_pickle(dataset_name) if os.path.exists(dataset_name) else pd.DataFrame()
+    stats = pd.read_pickle(stats_name) if os.path.exists(stats_name) else pd.DataFrame()
+
+    for problem in tqdm(problems, desc=f"Solving problems {name}", unit="problem"):
         try:
-            solution, stats_result = problem.solve()
-            dataset = pd.concat([dataset, solution], ignore_index=True)
-            stats_row  = pd.DataFrame.from_dict(stats_result, orient='index').T
-            stats = pd.concat([stats, stats_row], ignore_index=True)
-            print(f"Problem {problem.name} solved in {stats_result['time']} seconds")
+            names = stats['name'].values if stats.get('name', None) is not None else []
+            if not problem.name in names:
+                solution, stats_result = problem.solve()
+                dataset = pd.concat([dataset, solution], ignore_index=True)
+                stats_row  = pd.DataFrame.from_dict(stats_result, orient='index').T
+                stats = pd.concat([stats, stats_row], ignore_index=True)
+                print(f"Problem {problem.name} solved in {stats_result['time']} seconds")
+                # overwrite dataset and stats files
+                dataset.to_pickle(f"dataset/{name}_solution.pkl")
+                stats.to_pickle(f"dataset/{name}_stats.pkl")
+            else:
+                print(f"Problem {problem.name} already solved, skipping.")
+        except AssertionError as e:
+            print(f"Problem has no solution: {problem.name}")
         except Exception as e:
-            print(f"Error in problem {problem.name}: {e}")
+            print(f"Error solving problem {problem.name}: {e}")
+            continue
 
-    if dataset.empty:
-        return
-    dataset.to_pickle(f'dataset.pickle')
-    stats.to_pickle(f'stats.pickle')
+
+    dataset.to_pickle(f"dataset/{name}_solution.pkl")
+    stats.to_pickle(f"dataset/{name}_stats.pkl")
+
 
 if __name__ == "__main__":
-    load_and_export()
+    np.random.seed(42)
+
+    miplib = load_miplib_dataset()
+    generated = generate_datasets(
+        set_cover_instances=0,
+        bin_packing_instances=60,
+        traveling_salesman_instances=0,
+    )
+
+    # split in train and test (80% train, 20% test)
+    for name, problems in generated.items():
+        n_train = int(len(problems) * 0.8)
+        np.random.shuffle(problems)
+        generated[name] = {
+            'train': problems[:n_train],
+            'test': problems[n_train:]
+        }
+
+    # for problem in miplib:
+    #     print(f"Solving {problem.name} problem {datetime.now()}")
+    #     solve([problem], "miplib")
+
+    # sort miplib problems by size (number of variables)
+    # miplib.sort(key=lambda x: len(x.c), reverse=True)
+
+    # for problem in tqdm(miplib, desc="Solving miplib problems", unit="problem"):
+    #     print(f"Solving {problem.name} problem {datetime.now()}")
+    #     solve([problem], "miplib")
+
+
+    for name, problems in generated.items():
+        print(f"Solving {name} problems")
+        solve(problems['train'], f"{name}_train")
+        print(f"Solving {name} test problems")
+        solve(problems['test'], f"{name}_test")
+    
